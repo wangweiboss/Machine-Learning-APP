fcfea5b33619084d14fb3b2093034c99c4992f8a
wangweiboss <1017395252@qq.com>
2016-08-20 20:17:40 +0800

    SVD、PCA、eig以及A转置乘A和A乘A转置的特征向量之间的关系
    SVD的S和D是可以转换的
    
    eig(A'A)和eig(AA')的特征向量也是可以转换的

---
+++ SVD、PCA、eig以及A转置乘A和A乘A转置的特征向量之间的关系.m
@@ -0,0 +1,15 @@
+clc,clear
+load hald; %其中ingredients 为matlab自带  
+X = zscore(ingredients); 
+%% 方法1
+[pc, score, latent,tsquare]=pca(X); % 不会给你去均值，拿来就算协方差 (X'*X)./n
+%% 方法2
+cov_ingredients =cov(X);% cov(X)去了均值，所以不等于(X'*X)./12，要用cov，最好将原始数据标准化;  %(X'*X)./12;
+[V,D]=eig(cov_ingredients);    % D=latent,pc=v  
+
+cov_ingredients1 =(X*X')./3;% cov(X');%(X*X')./3;用cov会有偏差
+[V1,D1]=eig(cov_ingredients1);    
+%V1和V的关系：V1(i)=X*V(i)/vv(倒着来,倒着来) 因为D和D1是从小到大排的，vv是从大到小排的，所以要倒着
+%% 方法3
+[ss,vv,dd] = svd(X);  %ss==V1,vv^2==D*12==D1*3,dd==V
+%ss和dd的关系：ss(i)=X*dd(i)/vv(i,i)
